231214326ac8f421adae1ad7b1abb917dabc4ca9
hiel <yangjunghooon@gmail.com>
2024-11-28 09:43:42 +0900

    Initial Commit

---
+++ src/test/kotlin/com/hiel/commerce/CommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hiel.commerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommerceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
